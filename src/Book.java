11b50012ca594c1bbad13d718ff82a3c649018ad
LesnikovYV <Y-levl@ya.ru>
2022-05-27 00:23:58 +0500

    1

---
+++ src/Book.java
@@ -0,0 +1,36 @@
+public class Book {
+
+    private String title;
+    private Author autor;
+    private int year;
+
+    public Book(String title, Author autor, int year) {
+        this.autor = autor;
+        this.year = year;
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Author getAutor() {
+        return autor;
+    }
+
+    public void setAutor(Author autor) {
+        this.autor = autor;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        Author autor1 = new Author("Л.Н. ", "Толстой");
+        Author autor2 = new Author("М.Ю. ", "Лермонтв");
+        Author autor3 = new Author("А.С. ", "Пушкин");
+
+        Book book1 = new Book("Война и мир", autor1, 1805);
+        Book book2 = new Book("Мцири", autor2, 1838);
+        Book book3 = new Book("Евгений Онегин", autor3, 1830);
+
+        System.out.println(book1.getTitle() + " " + book1.getYear() + " " + book1.getAutor().getName()+""+book1.getAutor().getSurname());
+        System.out.println(book2.getTitle() + " " + book2.getYear() + " " + book1.getAutor().getName()+""+book2.getAutor().getSurname());
+        System.out.println(book3.getTitle() + " " + book3.getYear() + " " + book1.getAutor().getName()+""+book3.getAutor().getSurname());
+
+        book1.setYear(1812);
+        System.out.println(book1.getTitle() + " " + book1.getYear() + " " + book1.getAutor().getName()+""+book1.getAutor().getSurname());
+    }
+}
